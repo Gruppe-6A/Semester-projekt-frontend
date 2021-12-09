b968659af47c5709f0b58fec1293ae235262c502
Nicolai <cph-nm172@cphbusiness.dk>
2021-12-09 14:49:18 +0100

    Added US-5

--- src/components/SearchForCoin.js
+++ src/components/SearchForCoin.js
@@ -2,31 +2,108 @@ import { useState, useEffect } from "react";
 import facade from "../apiFacade";
 
 function SearchForCoin({ facade }) {
-  const initialState = { searchBox: "" };
-  const [searchCrypto, setSearchCrypto] = useState(initialState);
-
-  const updates = (data) => ({
-    crypto: data.name,
-    price: data.price,
-    exchange: data.exchange,
+  const [cryptoList, setCryptoList] = useState([
+    {
+      crypto: "",
+    },
+  ]);
+  const [searchShow, setSearchShow] = useState(false);
+  const [searchCrypto, setSearchCrypto] = useState("");
+  const [krakenCrypto, setKrakenCrypto] = useState({
+    crypto: "",
+    exchange: "",
+    price: "",
   });
 
+  const [yobitCrypto, setYobitCrypto] = useState({
+    crypto: "",
+    exchange: "",
+    price: "",
+  });
+
+  const updates1 = (data) => {
+    const compareCryptoList = [];
+    data.map((i) => {
+      compareCryptoList.push({
+        crypto: i.name,
+      });
+    });
+    setCryptoList(compareCryptoList);
+    console.log(cryptoList);
+  };
+
+  useEffect(() => {
+    facade.fetchData("crypto/cryptoList", updates1);
+  }, [facade]);
+
+  const updates = (data) => {
+    setYobitCrypto({
+      crypto: data[0].name,
+      exchange: data[0].exchange,
+      price: data[0].price,
+    });
+
+    setKrakenCrypto({
+      crypto: data[1].name,
+      exchange: data[1].exchange,
+      price: data[1].price,
+    });
+  };
+
   const handleSubmit = (evt) => {
-    alert(JSON.stringify(searchCrypto));
+    evt.preventDefault();
+    {
+      {
+        cryptoList
+          .filter((name) => name.crypto.includes(searchCrypto))
+          .map((filteredName) =>
+            facade.fetchData(
+              "crypto/cryptoList/" + filteredName.crypto,
+              updates
+            )
+          );
+      }
+    }
   };
 
   const handleInput = (e) => {
-    e.preventDefault();
     const target = e.target;
     const value = target.value;
     setSearchCrypto(value);
     console.log(searchCrypto);
+    if (value === searchCrypto) {
+      setSearchShow(false);
+    } else {
+      setSearchShow(true);
+    }
   };
+
+  function showDiv() {
+    if (searchShow) {
+      return (
+        <div>
+          <h3 onClick>
+            Prisen på {krakenCrypto.crypto} er ${krakenCrypto.price}, ifølge{" "}
+            {krakenCrypto.exchange}
+          </h3>
+          <h3>
+            Prisen på {yobitCrypto.crypto} er ${yobitCrypto.price}, ifølge{" "}
+            {yobitCrypto.exchange}
+          </h3>
+        </div>
+      );
+    }
+  }
 
   return (
     <div>
       <form onSubmit={handleSubmit} onChange={handleInput}>
-        <input type="text" placeholder="Fx. bitcoin" id="searchBox" />
+        <input
+          onChange={handleInput}
+          type="text"
+          placeholder="Fx. bitcoin"
+          id="searchBox"
+        />
         <div>
           <button
             onClick={handleSubmit}
@@ -37,6 +114,7 @@ function SearchForCoin({ facade }) {
           </button>
         </div>
       </form>
+      <div>{showDiv()}</div>
     </div>
   );
 }
